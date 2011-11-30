78fb0d10c9fa258efd592bdaaa6666877fd0ae1a
Makoto Inoue <inouemak@googlemail.com>
2011-11-30 07:07:45 +0000

    Changed label

--- public/app.coffee
+++ public/app.coffee
@@ -1,12 +1,12 @@
 $(document).ready ->
   new Radar(Raphael('aggregates', 300, 300)  , [
-    {score:4, label:'2.4%'},
-    {score:2, label:'2.3%'},
-    {score:5, label:'2.2%'},
-    {score:4, label:'2.1%'},
-    {score:5, label:'2.0%'}
+    {score:4, label:'Debt / GDP 2.4%'},
+    {score:2, label:'Deficit / GDP 2.3%'},
+    {score:5, label:'10 yr bond yield 2%'},
+    {score:4, label:'Enemployment 2%'},
+    {score:5, label:'GDP 2%'}
   ]).draw()
-
+  
   for i in ["BE","BG","CZ","DK","DE","EE","IE","GR","ES","FR","IT","CY","LV","LT","LU","HU","MT","NL","AT","PL","PT","RO","SI","SK","FI","SE","GB","IS","NO"]
     console.log(i)
     new Radar(Raphael('france', 90, 90)  , [
--- public/app.js
+++ public/app.js
@@ -4,19 +4,19 @@
     new Radar(Raphael('aggregates', 300, 300), [
       {
         score: 4,
-        label: '2.4%'
+        label: 'Debt / GDP 2.4%'
       }, {
         score: 2,
-        label: '2.3%'
+        label: 'Deficit / GDP 2.3%'
       }, {
         score: 5,
-        label: '2.2%'
+        label: '10 yr bond yield 2%'
       }, {
         score: 4,
-        label: '2.1%'
+        label: 'Enemployment 2%'
       }, {
         score: 5,
-        label: '2.0%'
+        label: 'GDP 2%'
       }
     ]).draw();
     _ref = ["BE", "BG", "CZ", "DK", "DE", "EE", "IE", "GR", "ES", "FR", "IT", "CY", "LV", "LT", "LU", "HU", "MT", "NL", "AT", "PL", "PT", "RO", "SI", "SK", "FI", "SE", "GB", "IS", "NO"];

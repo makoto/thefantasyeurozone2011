ca431b2410e6502b0cf2a68e86fb7bdf2c181593
Makoto Inoue <inouemak@googlemail.com>
2011-12-02 10:55:24 +0000

    Added eurozone gdp aggregate

--- public/barchart.coffee
+++ public/barchart.coffee
@@ -6,25 +6,37 @@ class Barchart
   draw: ()->
     gdp = @gdp
     r = Raphael('comparison', 200, 300)
-    eu = r.rect(30, 20, 20, 200)
+    length = 200
+    startY = 20
+    w = 20
+    eu = r.rect(30, 20, 20, length)
     eu.attr({ stroke:'#16617F', fill:'white'})
-    eurozone = r.rect(30, 120, 20, 100)
+
+    sum = 0
+    for a in Aggregate.all()
+      sum+=a.gdp
+    console.log('sum', sum)
+    ratio = (sum / @toBillion gdp[0][1]).toFixed(2)
+    console.log 'ratio', ratio
+    top = (length * (1 - ratio)) + startY
+    bottom =  length * ratio
+    x = 30
+
+    eurozone = r.rect(x, top, w, bottom)
     eurozone.attr({fill:'#16617F', stroke:'#16617F'})
-    [x, startY, w, h] = [80, 20, 20, 200]
-    rtotal = r.rect(x, startY, w, h)  
+
+    x = 80
+    rtotal = r.rect(x, startY, w, length)
     rtotal.attr({stroke:'#E34528'})
-    length = h # 200 - 20 = 180
     counter = 0
-    console.log('length', @gdp.length)
     textX = x + 70
-    
+
     for c in gdp[1..(gdp.length - 1)]
-      console.log('c', c, counter)
       ratio = c[1] / parseFloat(gdp[0][1])
       top = (length * (1 - ratio)) + startY
       bottom =  length * ratio
       a = r.rect(x, top, w, bottom)  
-      s = 1 * ((1 - (ratio / 1.5)) )
+      s = 1 * (ratio / 1.5)
       a.attr({stroke:'#E34528', fill:"hsb(0,#{s},1)"})
       r.text(textX, top, "#{gdp[counter][0]} #{@toBillion gdp[counter][1]}") if counter < 4 && counter != 0
       counter++
@@ -35,7 +47,7 @@ class Barchart
     for c in [5..13]
       texts.push "#{gdp[c][0]} #{@toBillion gdp[c][1]}"
     texts = texts.join('\n')
-    r.text(textX, 200, texts)
+    r.text(textX, length, texts)
     
   toBillion: (val) ->
     parseFloat( (val / 1000 / 1000 / 1000).toFixed(2))
--- public/barchart.js
+++ public/barchart.js
@@ -5,37 +5,49 @@
       this.gdp = gdp;
     }
     Barchart.prototype.draw = function() {
-      var a, bottom, c, counter, eu, eurozone, gdp, h, length, r, ratio, rtotal, s, startY, textX, texts, top, w, x, _i, _len, _ref, _ref2;
+      var a, bottom, c, counter, eu, eurozone, gdp, length, r, ratio, rtotal, s, startY, sum, textX, texts, top, w, x, _i, _j, _len, _len2, _ref, _ref2;
       gdp = this.gdp;
       r = Raphael('comparison', 200, 300);
-      eu = r.rect(30, 20, 20, 200);
+      length = 200;
+      startY = 20;
+      w = 20;
+      eu = r.rect(30, 20, 20, length);
       eu.attr({
         stroke: '#16617F',
         fill: 'white'
       });
-      eurozone = r.rect(30, 120, 20, 100);
+      sum = 0;
+      _ref = Aggregate.all();
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        a = _ref[_i];
+        sum += a.gdp;
+      }
+      console.log('sum', sum);
+      ratio = (sum / this.toBillion(gdp[0][1])).toFixed(2);
+      console.log('ratio', ratio);
+      top = (length * (1 - ratio)) + startY;
+      bottom = length * ratio;
+      x = 30;
+      eurozone = r.rect(x, top, w, bottom);
       eurozone.attr({
         fill: '#16617F',
         stroke: '#16617F'
       });
-      _ref = [80, 20, 20, 200], x = _ref[0], startY = _ref[1], w = _ref[2], h = _ref[3];
-      rtotal = r.rect(x, startY, w, h);
+      x = 80;
+      rtotal = r.rect(x, startY, w, length);
       rtotal.attr({
         stroke: '#E34528'
       });
-      length = h;
       counter = 0;
-      console.log('length', this.gdp.length);
       textX = x + 70;
       _ref2 = gdp.slice(1, (gdp.length - 1 + 1) || 9e9);
-      for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
-        c = _ref2[_i];
-        console.log('c', c, counter);
+      for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
+        c = _ref2[_j];
         ratio = c[1] / parseFloat(gdp[0][1]);
         top = (length * (1 - ratio)) + startY;
         bottom = length * ratio;
         a = r.rect(x, top, w, bottom);
-        s = 1 * (1 - (ratio / 1.5));
+        s = 1 * (ratio / 1.5);
         a.attr({
           stroke: '#E34528',
           fill: "hsb(0," + s + ",1)"
@@ -50,7 +62,7 @@
         texts.push("" + gdp[c][0] + " " + (this.toBillion(gdp[c][1])));
       }
       texts = texts.join('\n');
-      return r.text(textX, 200, texts);
+      return r.text(textX, length, texts);
     };
     Barchart.prototype.toBillion = function(val) {
       return parseFloat((val / 1000 / 1000 / 1000).toFixed(2));
